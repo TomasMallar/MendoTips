2cbecfb68e3d1ad8d40f3f2e2ff458520647d877
Tomas <mallartomas@gmail.com>
2022-03-18 13:17:11 -0300

    Cuarto Commit

---
+++ js/variablesForm.js
@@ -0,0 +1,10 @@
+const inputNombre = document.querySelector("#inputNombre")
+const inputTelefono = document.querySelector("#inputApellido")
+const inputEmail = document.querySelector("#inputEmail")
+const selectCheck = document.querySelector("#selectCheck")
+const checkbox = document.querySelector("#gridCheck")
+const exampleFormControlTextarea1 = document.querySelector("#exampleFormControlTextarea1")
+const submit = document.querySelector("#submit")
+const reset = document.querySelector("#reset")
+
+let datosDeInput = ""
