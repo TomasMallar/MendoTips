94fe1d58c89b93ec8f070ef93657ea9c16e2c632
Tomas <mallartomas@gmail.com>
2022-04-01 19:03:08 -0300

    sixth commit

--- js/variablesForm.js
+++ js/variablesForm.js
@@ -8,3 +8,51 @@ const submit = document.querySelector("#submit")
 const reset = document.querySelector("#reset")
 
 let datosDeInput = ""
+
+
+reset.addEventListener("click", ()=> {
+    Swal.fire({
+        title: "¿Realmente desea limpiar los campos de formulario?",
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonText: "Sip",
+        cancelButtonText: "Uh, no!"
+    }).then((result)=> {
+        result.isConfirmed ?
+        location.reload() :
+        nombre.focus()
+    })
+
+})
+
+submit.addEventListener("click", ()=> {
+    Swal.fire({
+        title: "Se envió correctamente",
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonText: "Enviar otro",
+        cancelButtonText: "Volver"
+    }).then((result)=> {
+        result.isConfirmed ?
+        location.reload() :
+        nombre.focus()
+    })
+
+})
+
+function mensajeToast() {
+    Toastify({
+        text: "Esta es una alerta",
+        duration: 3000,
+//        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top", 
+        position: "left", 
+        stopOnFocus: true,
+        style: {
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+        onClick: function(){} 
+      }).showToast();
+}
