9ffdb63e2eedb41c8848f5a5d90f24b2773ae299
TomasMallar <mallartomas@gmail.com>
2022-05-13 11:57:26 -0300

    commit correcciones

--- js/json.js
+++ js/json.js
@@ -1,23 +1,24 @@
-const guardoDatosJSON = ()=> {
-    debugger  
-    const datosForm = {id: "", promo: 0, Importe: 0}
-        datosForm.id = id.value
-        datosForm.promo = promo.value
-        datosForm.importe = importe.value
-        localStorage.setItem("datosDelForm", JSON.stringify(datosForm))
-        console.info("Se ha almacenado el array en LS.")
-}
+// const btnGuardar = document.getElementById('guardar');
+// const guardoDatosJSON = ()=> {
+//     debugger  
+//     const datosForm = {id: "", promo: 0, Importe: 0}
+//         datosForm.id = id.value
+//         datosForm.promo = promo.value
+//         datosForm.importe = importe.value
+//         localStorage.setItem("datosDelForm", JSON.stringify(datosForm))
+//         console.info("Se ha almacenado el array en LS.")
+// }
 
-btnGuardar.addEventListener("click", guardoDatosJSON)
+// btnGuardar.addEventListener("click", guardoDatosJSON)
 
 
-const recuperoDatosJSON = ()=> {
-    if (localStorage.getItem("datosDelForm") != null) {
-        datosDelForm = JSON.parse(localStorage.getItem("datosDelForm"))
-        id.value = datosDelForm.id
-        promo.value = datosDelForm.promo
-        importe.value = datosDelForm.importe
-        }
-}
+// const recuperoDatosJSON = ()=> {
+//     if (localStorage.getItem("datosDelForm") != null) {
+//         datosDelForm = JSON.parse(localStorage.getItem("datosDelForm"))
+//         id.value = datosDelForm.id
+//         promo.value = datosDelForm.promo
+//         importe.value = datosDelForm.importe
+//         }
+// }
 
-recuperoDatosJSON()
+// recuperoDatosJSON()
